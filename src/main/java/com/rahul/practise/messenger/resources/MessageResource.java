a95bb947a51c814417eb09773580454d27e5c89c
Rahul Derashri <rahul.derashri@gmail.com>
2015-10-25 12:14:44 -0400

    support for addMessage, updateMessage, deleteMessage and get specific message by id

--- src/main/java/com/rahul/practise/messenger/resources/MessageResource.java
+++ src/main/java/com/rahul/practise/messenger/resources/MessageResource.java
@@ -2,8 +2,13 @@ package com.rahul.practise.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -16,8 +21,38 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessage(){
 		return messageService.getMessages();
+	}
+	
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId") long id){
+		return messageService.getMessage(id);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message msg){
+		return messageService.addMessage(msg);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id , Message msg){
+		msg.setId(id);
+		return messageService.updateMessage(msg);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message deleteMessage(@PathParam("messageId") long id){
+		return messageService.removeMessage(id);
 	}
 }
--- src/main/java/com/rahul/practise/messenger/service/MessageService.java
+++ src/main/java/com/rahul/practise/messenger/service/MessageService.java
@@ -2,23 +2,50 @@ package com.rahul.practise.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import com.rahul.practise.messenger.database.Database;
 import com.rahul.practise.messenger.model.Message;
 
 public class MessageService {
-	public List<Message> getMessages(){
+	
+	private Map<Long, Message> messages = Database.getMessages();
+	
+	public MessageService() {
 		Message m1 = new Message(1L, "Message 1", "Rahul");
-		Message m2 = new Message(1L, "Message 2", "Rahul");
-		Message m3 = new Message(1L, "Message 3", "Rahul");
-		Message m4 = new Message(1L, "Message 4", "Rahul");
-		
-		List<Message> list = new ArrayList<Message>();
-		list.add(m1);
-		list.add(m2);
-		list.add(m3);
-		list.add(m4);
-		
-		return list;
+		Message m2 = new Message(2L, "Message 2", "Rahul");
+		Message m3 = new Message(3L, "Message 3", "Rahul");
+		Message m4 = new Message(4L, "Message 4", "Rahul");
+		messages.put(1l, m1);
+		messages.put(2l, m2);
+		messages.put(3l, m3);
+		messages.put(4l, m4);
 	}
 	
+	public List<Message> getMessages(){
+		return new ArrayList<Message>(messages.values());
+	}
+	
+	public Message getMessage(long id){
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message msg){
+		msg.setId(messages.size()+1);
+		messages.put( msg.getId(), msg);
+		return msg;
+	}
+	
+	public Message updateMessage(Message msg){
+		if( msg.getId() < 0 ){
+			return null;
+		}
+		
+		messages.put(msg.getId(), msg);
+		return msg;
+	}
+	
+	public Message removeMessage(long id){
+		return messages.remove(id);
+	}
 }
